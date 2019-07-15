73c82f71d778c6a98d02a0b83ed3174f13113e60
Jason Hicken <hickej2@rpi.edu>
2019-07-15 10:14:14 -0400

    moved euler fluxes into their own header file

--- src/utils.hpp
+++ src/utils.hpp
@@ -33,6 +33,40 @@ protected:
    std::string error_msg;
 };
 
+/// for performing loop unrolling of dot-products using meta-programming
+/// \tparam xdouble - `double` or `adept::adouble`
+/// \tparam dim - number of dimensions for array
+/// This was adapted from http://www.informit.com/articles/article.aspx?p=30667&seqNum=7
+template <typename xdouble, int dim>
+class DotProduct {
+  public:
+    static xdouble result(const xdouble *a, const xdouble *b)
+    {
+        return *a * *b  +  DotProduct<xdouble,dim-1>::result(a+1,b+1);
+    }
+};
+
+// partial specialization as end criteria
+template <typename xdouble>
+class DotProduct<xdouble,1> {
+  public:
+    static xdouble result(const xdouble *a, const xdouble *b)
+    {
+        return *a * *b;
+    }
+};
+
+/// dot product of two arrays that uses an unrolled loop
+/// \param[in] a - first vector involved in product
+/// \param[in] b - second vector involved in product
+/// \tparam xdouble - typically `double` or `adept::adouble`
+/// \tparam dim - number of array dimensions
+template <typename xdouble, int dim>
+inline xdouble dot(const xdouble *a, const xdouble *b)
+{
+    return DotProduct<xdouble,dim>::result(a,b);
+}
+
 } // namespace mach
 
 #endif 
