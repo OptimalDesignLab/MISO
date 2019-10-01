a6fa5be6bf0c6b3d3edfc9f61133ab258ffee569
Tucker Babcock <tbabcock03@gmail.com>
2019-10-01 11:31:19 -0400

    Added SFINAE code for MeshDependentCoefficient to evaluate EvalStateDeriv if the coefficient in the map is a subclass of ExplicitStateDependentCoefficient and thus implements it. TODO change from using map to unordered_map?

--- src/coefficient.cpp
+++ src/coefficient.cpp
@@ -5,6 +5,54 @@ using namespace mfem;
 namespace mach
 {
 
+double MeshDependentCoefficient::Eval(ElementTransformation &trans,
+                                      const IntegrationPoint &ip)
+{
+   // given the attribute, extract the coefficient value from the map
+   std::map<const int, Coefficient*>::iterator it;
+   int this_att = trans.Attribute;
+   Coefficient *coeff;
+	double value;
+   it = material_map.find(this_att);
+   if (it != material_map.end())
+   {
+      coeff = it->second;
+		value = coeff->Eval(trans, ip);
+   }
+   else
+   {
+      value = 0.0; // avoid compile warning
+      std::cerr << "MeshDependentCoefficient attribute " << it->first
+                << " not found" << std::endl;
+      mfem_error();
+   }
+   return value;
+}
+
+double MeshDependentCoefficient::EvalStateDeriv(ElementTransformation &trans,
+                                      				const IntegrationPoint &ip)
+{
+   // given the attribute, extract the coefficient value from the map
+   std::map<const int, Coefficient*>::iterator it;
+   int this_att = trans.Attribute;
+	double value;
+   it = material_map.find(this_att);
+   if (it != material_map.end())
+   {
+      Coefficient *coeff = it->second;
+		value = EvalStateDeriv(coeff, trans, ip);
+   }
+   else
+   {
+      value = 0.0; // avoid compile warning
+      std::cerr << "MeshDependentCoefficient attribute " << it->first
+                << " not found" << std::endl;
+      mfem_error();
+   }
+   return value;
+}
+
+
 double ReluctivityCoefficient::Eval(ElementTransformation &trans,
 												const IntegrationPoint &ip)
 {
--- src/coefficient.hpp
+++ src/coefficient.hpp
@@ -1,20 +1,171 @@
 #ifndef MACH_COEFFICIENT
 #define MACH_COEFFICIENT
 
+#include <map>
 #include "mfem.hpp"
 
 #include "mach_types.hpp"
+#include "utils.hpp"
 
 
 namespace mach
 {
+
+/// TODO - replace mfem_error with mach_error type
+class ExplictStateDependentCoefficient : public mfem::Coefficient
+{
+public:
+	virtual double EvalStateDeriv(mfem::ElementTransformation &trans,
+											const mfem::IntegrationPoint &ip)
+	{
+		mfem::mfem_error("ExplicitStateDependnetCoefficient is a pure abstact"
+								"class.");
+		return 0.0;
+	}
+};
+
+
+class MeshDependentCoefficient : public ExplictStateDependentCoefficient
+{
+public:
+	MeshDependentCoefficient()
+	{
+		// initialize empty material_map
+		material_map = {};
+
+		// // create `hasEvalStateDeriv` anonymous function
+		// const mfem::ElementTransformation &trans = mfem::IsoparametricTransformation();
+		// const mfem::IntegrationPoint &ip = mfem::IntegrationPoint();
+		// hasEvalStateDeriv = mach::is_valid([](auto&& x) -> decltype(x.EvalStateDeriv(trans, ip)) { });
+	}
+
+	MeshDependentCoefficient(const std::map<const int, mfem::Coefficient*> 
+									 &input_map)
+	 : material_map(input_map)
+	 {
+		// // create `hasEvalStateDeriv` anonymous function
+		// const mfem::ElementTransformation &trans = mfem::IsoparametricTransformation();
+		// const mfem::IntegrationPoint &ip = mfem::IntegrationPoint();
+		// hasEvalStateDeriv = mach::is_valid([](auto&& x) -> decltype(x.EvalStateDeriv(trans, ip)) { });
+	 }
+	
+	/// Adds <int, mfem::Coefficient*> pair to material_map where the int
+	/// corresponds to a material attribute in the mesh.
+	/// \param coeff - attribute-Coefficient pair where the Coefficient is the
+	///					 one to evaluate on elements identified by the attribute
+	virtual void addCoefficient(std::pair<const int, mfem::Coefficient*> coeff)
+	{
+		std::pair <std::map<const int, mfem::Coefficient*>::iterator, bool> status; 
+		status = material_map.insert(coeff);
+		// if the pair failed to insert
+		if (!status.second)
+		{
+			mfem::mfem_error("Key already present in map!");
+		}
+	}
+
+	/// \brief Search the map of coefficients and evaluate the one whose key is
+	/// 		  the same as the element's `Attribute` at the point defined by 
+	///		  `ip`.
+	/// \param[in] trans - element transformation relating real element to 
+	///					 	  reference element
+	/// \param[in] ip - the integration point to evalaute the coefficient at
+   /// \note When this method is called, the caller must make sure that the
+   /// IntegrationPoint associated with trans is the same as ip. This can be
+   /// achieved by calling trans.SetIntPoint(&ip).
+	virtual double Eval(mfem::ElementTransformation &trans,
+							  const mfem::IntegrationPoint &ip);
+	
+	/// TODO - implement expression SFINAE when iterating over map
+	/// TODO - Consider different model for coefficient's dependent upon multiple
+	///		  GridFunctions
+	/// \brief Search the map of coefficients and evaluate the derivative with 
+	/// 		  respect to the state of the one whose key is the same as the 
+	///		  element's `Attribute` at the point defined by `ip`.
+	/// \param[in] trans - element transformation relating real element to 
+	///					 	  reference element
+	/// \param[in] ip - the integration point to evalaute the coefficient at
+   /// \note When this method is called, the caller must make sure that the
+   /// IntegrationPoint associated with trans is the same as ip. This can be
+   /// achieved by calling trans.SetIntPoint(&ip).
+	virtual double EvalStateDeriv(mfem::ElementTransformation &trans,
+											const mfem::IntegrationPoint &ip);
+
+protected:
+	// template <class T> auto EvalStateDeriv(T& coeff, 
+	// 													mfem::ElementTransformation &trans, 
+	// 													const mfem::IntegrationPoint &ip)
+	// 		-> typename std::enable_if<decltype(hasEvalStateDeriv(coeff))::value,
+	// 											double>::type
+	// {
+	// 	return coeff.EvalStateDeriv(trans,ip);
+	// }
+
+	// template <class T> auto EvalStateDeriv(T& coeff, 
+	// 													mfem::ElementTransformation &trans,
+	// 													const mfem::IntegrationPoint &ip)
+	// 		-> typename std::enable_if<!decltype(hasEvalStateDeriv(coeff))::value,
+	// 											double>::type
+	// {
+	// 	return 0.0;
+	// }
+
+
+	/// \brief Method to be called if a coefficient matching the element's
+	/// 		  attribute is a subclass of `ExplicitStateDependentCoefficient and
+	///		  thus implements `EvalStateDeriv()`
+	/// \param[in] *coeff - pointer to the coefficient in the map
+	/// \param[in] trans - element transformation relating real element to 
+	///					 	  reference element
+	/// \param[in] ip - the integration point to evalaute the coefficient at
+	/// \tparam T - templated type, must be a subclass of `mfem::Coefficient`
+	/// \tparam typename - Uses template meta programming and SFINAE to check if
+	///						  `T` is a subclass of `ExplictStateDependentCoefficient`
+	///						  If it is not, typename is void and this function is
+	///						  an invalid overload and not considered. This enables
+	///						  compile-time introspection of object.
+   /// \note When this method is called, the caller must make sure that the
+   /// IntegrationPoint associated with trans is the same as ip. This can be
+   /// achieved by calling trans.SetIntPoint(&ip).
+	template <class T, typename std::enable_if<std::is_base_of<ExplictStateDependentCoefficient, T>::value, int>::type= 0>
+	inline double EvalStateDeriv(T *coeff, mfem::ElementTransformation &trans, const mfem::IntegrationPoint &ip)
+	{  
+		return coeff->EvalStateDeriv(trans, ip);
+	}
+
+	/// \brief Method to be called if a coefficient matching the element's
+	/// 		  attribute is not a subclass of `ExplicitStateDependentCoefficient
+	///		  and thus implements `EvalStateDeriv()`
+	/// \param[in] *coeff - pointer to the coefficient in the map
+	/// \param[in] trans - element transformation relating real element to 
+	///					 	  reference element
+	/// \param[in] ip - the integration point to evalaute the coefficient at
+	/// \tparam T - templated type, must be a subclass of `mfem::Coefficient`
+	/// \tparam typename - Uses template meta programming and SFINAE to check if
+	///						  `T` is a subclass of `ExplictStateDependentCoefficient`
+	///						  If it is not, typename is void and this function is
+	///						  an invalid overload and not considered. This enables
+	///						  compile-time introspection of object.
+   /// \note When this method is called, the caller must make sure that the
+   /// IntegrationPoint associated with trans is the same as ip. This can be
+   /// achieved by calling trans.SetIntPoint(&ip).
+	template <class T, typename std::enable_if<!std::is_base_of<ExplictStateDependentCoefficient, T>::value, int>::type = 0>
+	inline double EvalStateDeriv(T *coeff, mfem::ElementTransformation &trans, const mfem::IntegrationPoint &ip)	
+	{
+		return 0.0;
+	}
+
+private:
+	std::map<const int, mfem::Coefficient*> material_map;
+	// static const auto hasEvalStateDeriv;
+};
 
 // should i make this associated with a grid function or should I pass the state to it to evaluate?
 // need to check how Joule handles solving for temp, see if the state passed to an integrator is unique
 // to a finite element space (A and T would be seperate spaces). Also need to look at how much reluctivity
 // changes with temperature. (Ask prof Shah?) 
 
-class ReluctivityCoefficient : public mfem::Coefficient
+class ReluctivityCoefficient : public ExplictStateDependentCoefficient
 {
 public:
 	/// Define a temperature independent reluctivity model
