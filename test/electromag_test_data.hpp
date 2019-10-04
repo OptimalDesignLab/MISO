405a4d10cb8c128f43b71e53a3bd9a9f606f0e31
Tucker Babcock <tbabcock03@gmail.com>
2019-10-04 13:06:56 -0400

    Added nonlinear coefficient test case, based on e^(-norm(state)) and it's derivative. Tests between AssembleElementVector and AssembleElementGrad still fail, needs further investigation, probably starting with AssembleElementVector since it failed for the linear case when AssembleElementGrad didn't.

--- test/electromag_test_data.hpp
+++ test/electromag_test_data.hpp
@@ -5,6 +5,8 @@
 #include <random>
 #include "mfem.hpp"
 
+#include "coefficient.hpp"
+
 namespace electromag_data
 {
 // define the random-number generator; uniform between 0 and 1
@@ -12,17 +14,15 @@ static std::default_random_engine gen;
 static std::uniform_real_distribution<double> uniform_rand(-1.0,1.0);
 
 // template <int dim>
-// void randBaselinePert(const mfem::Vector &x, mfem::Vector &u)
-// {
-//    const double scale = 0.01;
-//    u(0) = rho*(1.0 + scale*uniform_rand(gen));
-//    u(dim+1) = rhoe*(1.0 + scale*uniform_rand(gen));
-//    for (int di = 0; di < dim; ++di)
-//    {
-//       u(di+1) = rhou[di]*(1.0 + scale*uniform_rand(gen));
-//    }
-// }
-// // explicit instantiation of the templated function above
+void randBaselinePert(const mfem::Vector &x, mfem::Vector &u)
+{
+   const double scale = 0.01;
+   for (int i = 0; i < u.Size(); ++i)
+   {
+      u(i) = (1.0 + scale*uniform_rand(gen));
+   }
+}
+// explicit instantiation of the templated function above
 // template void randBaselinePert<1>(const mfem::Vector &x, mfem::Vector &u);
 // template void randBaselinePert<2>(const mfem::Vector &x, mfem::Vector &u);
 // template void randBaselinePert<3>(const mfem::Vector &x, mfem::Vector &u);
@@ -46,6 +46,57 @@ void randState(const mfem::Vector &x, mfem::Vector &u)
 //    return uniform_rand(gen);
 //    // }
 // }
+
+/// Simple linear coefficient for testing CurlCurlNLFIntegrator
+class LinearCoefficient : public mach::ExplicitStateDependentCoefficient
+{
+public:
+	LinearCoefficient(double val = 1.0) : value(val) {}
+
+	double Eval(mfem::ElementTransformation &trans,
+					const mfem::IntegrationPoint &ip) override
+	{
+		return value;
+	}
+
+	double EvalStateDeriv(mfem::ElementTransformation &trans,
+								 const mfem::IntegrationPoint &ip) override
+	{
+		return 0.0;
+	}
+
+private:
+	double value;
+};
+
+/// Simple nonlinear coefficient for testing CurlCurlNLFIntegrator
+class NonLinearCoefficient : public mach::ExplicitStateDependentCoefficient
+{
+public:
+	NonLinearCoefficient(mfem::GridFunction *state_)
+	 : stateGF(state_) {}
+
+	double Eval(mfem::ElementTransformation &trans,
+					const mfem::IntegrationPoint &ip) override
+	{
+		mfem::Vector state;
+		stateGF->GetVectorValue(trans.ElementNo, ip, state);
+		double state_mag = state.Norml2();
+		return std::exp(-state_mag);
+	}
+
+	double EvalStateDeriv(mfem::ElementTransformation &trans,
+								 const mfem::IntegrationPoint &ip) override
+	{
+		mfem::Vector state;
+		stateGF->GetVectorValue(trans.ElementNo, ip, state);
+		double state_mag = state.Norml2();
+		return -std::exp(-state_mag);
+	}
+
+private:
+	mfem::GridFunction *stateGF;
+};
 
 } // namespace electromag_data
 
--- test/test_electromag_integ.cpp
+++ test/test_electromag_integ.cpp
@@ -33,15 +33,15 @@ TEST_CASE("CurlCurlNLFIntegrator::AssembleElementGrad - linear", "Works for line
                          
          NonlinearForm res(fes.get());
          
-         std::unique_ptr<mach::ExplictStateDependentCoefficient> nu(
-            new mach::ExplictStateDependentCoefficient());
+         std::unique_ptr<mach::ExplicitStateDependentCoefficient> nu(
+            new LinearCoefficient(1.0));
 
          res.AddDomainIntegrator(new mach::CurlCurlNLFIntegrator(nu.get()));
 
          // initialize state; here we randomly perturb a constant state
          GridFunction q(fes.get());
          // I think this should be 1? Only have one DOF per "node"
-         VectorFunctionCoefficient pert(1, randState);
+         VectorFunctionCoefficient pert(1, randBaselinePert);
          q.ProjectCoefficient(pert);
 
          // initialize the vector that the Jacobian multiplies
@@ -97,15 +97,78 @@ TEST_CASE("CurlCurlNLFIntegrator::AssembleElementGrad", "[CurlCurlNLFIntegrator]
                          
          NonlinearForm res(fes.get());
 
-         std::unique_ptr<mach::ExplictStateDependentCoefficient> nu(
-            new mach::ExplictStateDependentCoefficient());
+         std::unique_ptr<mach::ExplicitStateDependentCoefficient> nu(
+            new LinearCoefficient());
 
          res.AddDomainIntegrator(new mach::CurlCurlNLFIntegrator(nu.get()));
 
          // initialize state; here we randomly perturb a constant state
          GridFunction q(fes.get());
-         VectorFunctionCoefficient pert(1, randState);
+         VectorFunctionCoefficient pert(1, randBaselinePert);
          q.ProjectCoefficient(pert);
+
+         // initialize the vector that the Jacobian multiplies
+         GridFunction v(fes.get());
+         VectorFunctionCoefficient v_rand(1, randState);
+         v.ProjectCoefficient(v_rand);
+
+         // evaluate the Jacobian and compute its product with v
+         Operator& Jac = res.GetGradient(q);
+         GridFunction jac_v(fes.get());
+         Jac.Mult(v, jac_v);
+
+         // now compute the finite-difference approximation...
+         GridFunction q_pert(q), r(fes.get()), jac_v_fd(fes.get());
+         q_pert.Add(-delta, v);
+         res.Mult(q_pert, r);
+         q_pert.Add(2*delta, v);
+         res.Mult(q_pert, jac_v_fd);
+         jac_v_fd -= r;
+         jac_v_fd /= (2*delta);
+
+         for (int i = 0; i < jac_v.Size(); ++i)
+         {
+            REQUIRE( jac_v(i) == Approx(jac_v_fd(i)) );
+         }
+      }
+   }
+}
+
+TEST_CASE("CurlCurlNLFIntegrator::AssembleElementGrad - Nonlinear", "[CurlCurlNLFIntegrator]")
+{
+   using namespace mfem;
+   using namespace electromag_data;
+
+   const int dim = 3;  // templating is hard here because mesh constructors
+   // static adept::Stack diff_stack;
+   double delta = 1e-5;
+
+   // generate a 6 element mesh
+   int num_edge = 1;
+   std::unique_ptr<Mesh> mesh(new Mesh(num_edge, num_edge, num_edge, 
+                              Element::TETRAHEDRON, true /* gen. edges */, 1.0,
+                              1.0, 1.0, true));
+                              
+   for (int p = 1; p <= 4; ++p)
+   {
+      DYNAMIC_SECTION( "...for degree p = " << p )
+      {
+         std::unique_ptr<FiniteElementCollection> fec(
+            new ND_FECollection(p, dim));
+         std::unique_ptr<FiniteElementSpace> fes(new FiniteElementSpace(
+            mesh.get(), fec.get()));
+                         
+         NonlinearForm res(fes.get());
+
+         // initialize state; here we randomly perturb a constant state
+         GridFunction q(fes.get());
+         VectorFunctionCoefficient pert(1, randBaselinePert);
+         q.ProjectCoefficient(pert);
+
+         std::unique_ptr<mach::ExplicitStateDependentCoefficient> nu(
+            new NonLinearCoefficient(&q));
+
+         res.AddDomainIntegrator(new mach::CurlCurlNLFIntegrator(nu.get()));
 
          // initialize the vector that the Jacobian multiplies
          GridFunction v(fes.get());
