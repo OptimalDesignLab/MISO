c1cfd2a5399f663c36ba4245bc4783efaa541562
Aerokaur <kaursharanjeet977@gmail.com>
2019-10-23 13:50:45 -0400

    added file for patch test, code not complete yet

--- src/utils.cpp
+++ src/utils.cpp
@@ -9,30 +9,30 @@ namespace mach
 /// performs the Hadamard (elementwise) product: `v(i) = v1(i)*v2(i)`
 void multiplyElementwise(const Vector &v1, const Vector &v2, Vector &v)
 {
-   MFEM_ASSERT( v1.Size() == v2.Size() && v1.Size() == v.Size(), "");
+   MFEM_ASSERT(v1.Size() == v2.Size() && v1.Size() == v.Size(), "");
    for (int i = 0; i < v.Size(); ++i)
    {
-      v(i) = v1(i)*v2(i);
+      v(i) = v1(i) * v2(i);
    }
 }
 
 /// performs an elementwise division: `v(i) = v1(i)/v2(i)`
 void divideElementwise(const Vector &v1, const Vector &v2, Vector &v)
 {
-   MFEM_ASSERT( v1.Size() == v2.Size() && v1.Size() == v.Size(), "");
+   MFEM_ASSERT(v1.Size() == v2.Size() && v1.Size() == v.Size(), "");
    for (int i = 0; i < v.Size(); ++i)
    {
-      v(i) = v1(i)/v2(i);
+      v(i) = v1(i) / v2(i);
    }
 }
 
 /// performs elementwise inversion: `y(i) = 1/x(i)`
 void invertElementwise(const Vector &x, Vector &y)
 {
-   MFEM_ASSERT( x.Size() == y.Size(), "");
+   MFEM_ASSERT(x.Size() == y.Size(), "");
    for (int i = 0; i < x.Size(); ++i)
    {
-      y(i) = 1.0/x(i);
+      y(i) = 1.0 / x(i);
    }
 }
 
@@ -41,28 +41,28 @@ double quadInterp(double x0, double y0, double dydx0, double x1, double y1)
 {
    // Assume the fuction has the form y(x) = c0 + c1 * x + c2 * x^2
    double c0, c1, c2;
-   c0 = (dydx0*x0*x0*x1 + y1*x0*x0 - dydx0*x0*x1*x1 - 2*y0*x0*x1 + y0*x1*x1) /
-        (x0*x0 - 2*x1*x0 + x1*x1);
-   c1 = (2*x0*y0 - 2*x0*y1 - x0*x0*dydx0 + x1*x1*dydx0) / 
-        (x0*x0 - 2*x1*x0 + x1*x1);
-   c2 = -(y0 - y1 - x0*dydx0 + x1*dydx0) / (x0*x0 - 2*x1*x0 + x1*x1);
-   return -c1 / (2*c2);
+   c0 = (dydx0 * x0 * x0 * x1 + y1 * x0 * x0 - dydx0 * x0 * x1 * x1 - 2 * y0 * x0 * x1 + y0 * x1 * x1) /
+        (x0 * x0 - 2 * x1 * x0 + x1 * x1);
+   c1 = (2 * x0 * y0 - 2 * x0 * y1 - x0 * x0 * dydx0 + x1 * x1 * dydx0) /
+        (x0 * x0 - 2 * x1 * x0 + x1 * x1);
+   c2 = -(y0 - y1 - x0 * dydx0 + x1 * dydx0) / (x0 * x0 - 2 * x1 * x0 + x1 * x1);
+   return -c1 / (2 * c2);
 }
 
 #ifndef MFEM_USE_LAPACK
-   mfem::mfem_error(" Lapack is required for this feature ");
+mfem::mfem_error(" Lapack is required for this feature ");
 #else
 extern "C" void
 dgelss_(int *, int *, int *, double *, int *, double *, int *, double *,
-      double *, int *, double *, int *, int *);
+        double *, int *, double *, int *, int *);
 extern "C" void
 dgels_(char *, int *, int *, int *, double *, int *, double *, int *, double *,
-      int *, int *);
+       int *, int *);
 #endif
 /// build the interpolation operator on element patch
 /// this function will be moved later
 void buildInterpolation(int degree, const DenseMatrix &x_center,
-    const DenseMatrix &x_quad, DenseMatrix &interp)
+                        const DenseMatrix &x_quad, DenseMatrix &interp)
 {
    // number of quadrature points
    int num_quad = x_quad.Width();
@@ -74,35 +74,36 @@ void buildInterpolation(int degree, const DenseMatrix &x_center,
    int n = num_el;
 
    // Set the size of interpolation operator
-   interp.SetSize(num_quad,num_el);
+   interp.SetSize(num_quad, num_el);
    // required by the lapack routine
-   mfem::DenseMatrix rhs(n,1); 
+   mfem::DenseMatrix rhs(n, 1);
    char TRANS = 'N';
    int nrhs = 1;
-   int lwork = 2*m*n;
+   int lwork = 2 * m * n;
    double work[lwork];
 
    // construct each row of R (also loop over each quadrature point)
-   for(int i = 0; i < num_quad; i++)
+   for (int i = 0; i < num_quad; i++)
    {
       // reset the rhs
-      rhs = 0.0; rhs(0,0) = 1.0;
+      rhs = 0.0;
+      rhs(0, 0) = 1.0;
       // construct the aux matrix to solve each row of R
       DenseMatrix r(m, n);
       r = 0.0;
       // loop over each column of r
-      for(int j = 0; j < n; j++)
+      for (int j = 0; j < n; j++)
       {
-         double x_diff = x_center(0,j) - x_quad(0,i);
-         double y_diff = x_center(1,j) - x_quad(1,i);
-         r(0,j) = 1.0;
+         double x_diff = x_center(0, j) - x_quad(0, i);
+         double y_diff = x_center(1, j) - x_quad(1, i);
+         r(0, j) = 1.0;
          int index = 1;
          // loop over different orders
-         for(int order = 1; order <= degree; order++)
+         for (int order = 1; order <= degree; order++)
          {
-            for(int c = order; c >= 0; c--)
+            for (int c = order; c >= 0; c--)
             {
-               r(index, j) = pow(x_diff,c) * pow(y_diff, order-c);
+               r(index, j) = pow(x_diff, c) * pow(y_diff, order - c);
                index++;
             }
          }
@@ -113,10 +114,11 @@ void buildInterpolation(int degree, const DenseMatrix &x_center,
              work, &lwork, &info);
       MFEM_ASSERT(info == 0, "Fail to solve the underdetermined system.\n");
       // put each row back to interp
-      for(int k = 0; k < n; k++)
+      for (int k = 0; k < n; k++)
       {
-         interp(i,k) = rhs(k,0);
+         interp(i, k) = rhs(k, 0);
       }
-   } // end of constructing interp
+   }
+} // end of constructing interp
 
 } // namespace mach
---
+++ test/patch.cpp
@@ -0,0 +1,91 @@
+// Using this to test different ideas
+
+#include "mfem.hpp"
+#include "advection.hpp"
+#include <fstream>
+#include <iostream>
+#include "ao_fespace.hpp"
+
+using namespace std;
+using namespace mfem;
+using namespace mach;
+
+/// Defines the velocity field
+/// \param[in] x - coordinate of the point at which the velocity is needed
+/// \param[out] v - velocity components at \a x
+void velocity_function(const Vector &x, Vector &v);
+
+/// \brief Defines the initial condition
+/// \param[in] x - coordinate of the point at which the velocity is needed
+/// \param[out] u0 - scalar initial condition stored as a 1-vector
+void u0_function(const Vector &x, Vector& u0);
+
+int main(int argc, char *argv[])
+{
+   ostream *out;
+#ifdef MFEM_USE_MPI
+   // Initialize MPI if parallel
+   MPI_Init(&argc, &argv);
+   int rank;
+   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+   out = getOutStream(rank); 
+#else
+   out = getOutStream(0);
+#endif
+
+   // Parse command-line options
+   OptionsParser args(argc, argv);
+   const char *options_file = "mach_options.json";
+   args.AddOption(&options_file, "-o", "--options",
+                  "Options file to use.");
+   args.Parse();
+   if (!args.Good())
+   {
+      args.PrintUsage(cout);
+      return 1;
+   }
+
+   try
+   {
+      // construct the solver, set the initial condition, and solve
+      string opt_file_name(options_file);
+      mfem::ArbitraryOrderFESpace aospace(opt_file_name);
+   }
+   catch (MachException &exception)
+   {
+      exception.print_message();
+   }
+   catch (std::exception &exception)
+   {
+      cerr << exception.what() << endl;
+   }
+#ifdef MFEM_USE_MPI
+   MPI_Finalize();
+#endif
+}
+
+void velocity_function(const Vector &x, Vector &v)
+{
+   // Simply advection to upper right corner; See mfem ex9 to see how this might
+   // be generalized.
+   v(0) = 1.0;
+   v(1) = 1.0;
+}
+
+// Initial condition
+void u0_function(const Vector &x, Vector& u0)
+{
+   u0.SetSize(1);
+   double r2 = pow(x(0) - 0.5, 2.0) + pow(x(1) - 0.5, 2.0);
+   r2 *= 4.0;
+   if (r2 > 1.0)
+   {
+      u0(0) = 1.0;
+   } 
+   else
+   {
+      // the following is an expansion of u = 1.0 - (r2 - 1.0).^5
+      u0(0) = 2 - 5*r2 + 10*pow(r2,2) - 10*pow(r2,3) + 5*pow(r2,4) - pow(r2,5);
+   }
+}
+
