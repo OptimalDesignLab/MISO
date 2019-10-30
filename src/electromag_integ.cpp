70174ebb028a19104b117be382c9d4ae535d3e34
Tucker Babcock <tbabcock03@gmail.com>
2019-10-30 17:08:04 -0400

    added new magnetization nonlinear form integrator, soon to write test.

--- src/electromag_integ.cpp
+++ src/electromag_integ.cpp
@@ -181,4 +181,176 @@ void CurlCurlNLFIntegrator::AssembleElementGrad(
    }
 }
 
+void MagnetizationIntegrator::AssembleElementVector(
+    const FiniteElement &el,
+    ElementTransformation &trans,
+    const Vector &elfun, Vector &elvect)
+{
+	/// number of degrees of freedom
+	int ndof = el.GetDof();
+   int dim = el.GetDim();
+
+	/// I believe this takes advantage of a 2D problem not having
+	/// a properly defined curl? Need more investigation
+   int dimc = (dim == 3) ? 3 : 1;
+
+   /// holds quadrature weight
+   double w;
+
+#ifdef MFEM_THREAD_SAFE
+   DenseMatrix curlshape(ndof,dimc), curlshape_dFt(ndof,dimc), M;
+   Vector b_vec(dimc) mag_vec(dimc);
+#else
+   curlshape.SetSize(ndof,dimc);
+   curlshape_dFt.SetSize(ndof,dimc);
+   b_vec.SetSize(dimc);
+#endif
+
+	elvect.SetSize(ndof);
+
+	const IntegrationRule *ir = IntRule;
+   if (ir == NULL)
+   {
+      int order;
+      if (el.Space() == FunctionSpace::Pk)
+      {
+         order = 2*el.GetOrder() - 2;
+      }
+      else
+      {
+         order = 2*el.GetOrder();
+      }
+
+      ir = &IntRules.Get(el.GetGeomType(), order);
+   }
+
+   elvect = 0.0;
+
+	for (int i = 0; i < ir->GetNPoints(); i++)
+   {
+      const IntegrationPoint &ip = ir->IntPoint(i);
+
+      trans.SetIntPoint(&ip);
+
+      w = ip.weight / trans.Weight();
+
+      if ( dim == 3 )
+      {
+         el.CalcCurlShape(ip, curlshape);
+         MultABt(curlshape, trans.Jacobian(), curlshape_dFt);
+      }
+      else
+      {
+         el.CalcCurlShape(ip, curlshape_dFt);
+      }
+
+      b_vec = 0.0;
+      curlshape_dFt.AddMultTranspose(elfun, b_vec);
+      double nu_val = nu->Eval(trans, ip, b_vec.Norml2());
+      nu_val *= w;
+
+      mag_vec = 0.0;
+      mag->Eval(mag_vec, trans, ip);
+      mag_vec *= nu_val;
+      curlshape_dFt.AddMult(mag_vec, elvect);
+   }
+}
+
+void MagnetizationIntegrator::AssembleElementGrad(
+    const mfem::FiniteElement &el,
+    mfem::ElementTransformation &trans,
+    const mfem::Vector &elfun,
+    mfem::DenseMatrix &elmat)
+{
+   /// number of degrees of freedom
+	int ndof = el.GetDof();
+   int dim = el.GetDim();
+
+	/// I believe this takes advantage of a 2D problem not having
+	/// a properly defined curl? Need more investigation
+   int dimc = (dim == 3) ? 3 : 1;
+
+   /// holds quadrature weight
+   double w;
+
+#ifdef MFEM_THREAD_SAFE
+   DenseMatrix curlshape(ndof,dimc), curlshape_dFt(ndof,dimc);
+   Vector b_vec(dimc), mag_vec(dimc), temp_vec(ndof), temp_vec2(ndof);
+#else
+   curlshape.SetSize(ndof,dimc);
+   curlshape_dFt.SetSize(ndof,dimc);
+   b_vec.SetSize(dimc);
+   mag_vec.SetSize(dimc);
+   temp_vec.SetSize(ndof);
+   temp_vec2.SetSize(ndof);
+#endif
+
+	elmat.SetSize(ndof);
+
+	const IntegrationRule *ir = IntRule;
+   if (ir == NULL)
+   {
+      int order;
+      if (el.Space() == FunctionSpace::Pk)
+      {
+         order = 2*el.GetOrder() - 2;
+      }
+      else
+      {
+         order = 2*el.GetOrder();
+      }
+
+      ir = &IntRules.Get(el.GetGeomType(), order);
+   }
+
+   elmat = 0.0;
+	for (int i = 0; i < ir->GetNPoints(); i++)
+   {
+      const IntegrationPoint &ip = ir->IntPoint(i);
+
+      trans.SetIntPoint(&ip);
+
+      w = ip.weight / trans.Weight();
+
+      if ( dim == 3 )
+      {
+         el.CalcCurlShape(ip, curlshape);
+         MultABt(curlshape, trans.Jacobian(), curlshape_dFt);
+      }
+      else
+      {
+         el.CalcCurlShape(ip, curlshape_dFt);
+      }
+
+      /// calculate B = curl(A)
+      b_vec = 0.0;
+      curlshape_dFt.MultTranspose(elfun, b_vec);
+      const double b_mag = b_vec.Norml2();
+
+      /// TODO - is this thread safe?
+      /// calculate curl(N_i) dot curl(A), need to store in a DenseMatrix so we
+      /// can take outer product of result to generate matrix
+      temp_vec = 0.0;
+      curlshape_dFt.Mult(b_vec, temp_vec);
+      DenseMatrix temp_matrix(temp_vec.GetData(), ndof, 1);
+
+      mag_vec = 0.0;
+      mag->Eval(mag_vec, trans, ip);
+
+      temp_vec2 = 0.0;
+      curlshape_dFt.Mult(mag_vec, temp_vec);
+      DenseMatrix temp_matrix2(temp_vec.GetData(), ndof, 1);
+
+      /// evaluate the derivative of the material model with respect to the
+      /// norm of the grid function associated with the model at the point
+      /// defined by ip, and scale by integration point weight
+      double nu_deriv = nu->EvalStateDeriv(trans, ip, b_mag);
+      nu_deriv *= w;
+      nu_deriv /= b_mag;
+
+      /// add second term to elmat
+      AddMult_a_ABt(nu_deriv, temp_matrix, temp_matrix2, elmat);
+   }
+}
+
 } // namespace mach
--- src/electromag_integ.hpp
+++ src/electromag_integ.hpp
@@ -53,6 +53,52 @@ private:
 
 };
 
+/// Integrator for (\nu(u) M, curl v) for Nedelec Elements
+class MagnetizationIntegrator : public mfem::NonlinearFormIntegrator
+{
+public:
+   /// Construct a curl curl nonlinear form integrator for Nedelec elements
+	/// \param[in] m - model describing nonlinear material parameter
+   /// \param[in] a - used to move to lhs or rhs
+   MagnetizationIntegrator(StateCoefficient *nu,
+                           mfem::VectorCoefficient *M,
+								   double a = 1.0)
+		: nu(nu), mag(M), alpha(a) {}
+
+   /// Construct the element local residual
+   /// \param[in] el - the finite element whose residual we want
+   /// \param[in] trans - defines the reference to physical element mapping
+   /// \param[in] elfun - element local state vector
+   /// \param[out] elvect - element local residual
+   virtual void AssembleElementVector(const mfem::FiniteElement &el,
+                                      mfem::ElementTransformation &trans,
+                                      const mfem::Vector &elfun,
+                                      mfem::Vector &elvect);
+
+   /// Construct the element local Jacobian
+   /// \param[in] el - the finite element whose Jacobian we want
+   /// \param[in] trans - defines the reference to physical element mapping
+   /// \param[in] elfun - element local state vector
+   /// \param[out] elmat - element local Jacobian
+   virtual void AssembleElementGrad(const mfem::FiniteElement &el,
+                                    mfem::ElementTransformation &trans,
+                                    const mfem::Vector &elfun,
+                                    mfem::DenseMatrix &elmat);
+
+private:
+	/// material (thus mesh) dependent model for reluvtivity
+	StateCoefficient *nu;
+   /// material thus mesh dependent model for Magnetization
+   mfem::VectorCoefficient *mag;
+   /// scales the terms; can be used to move to rhs/lhs
+	double alpha;
+
+#ifndef MFEM_THREAD_SAFE
+   mfem::DenseMatrix curlshape, curlshape_dFt;
+   mfem::Vector b_vec, mag_vec, temp_vec, temp_vec2;
+#endif
+};
+
 } // namespace mach
 
 #endif
