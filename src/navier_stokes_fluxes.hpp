19bd0f5be68de7a49b397d32b484adfe9c7b2ff4
Sharanjeet Kaur <kaursharanjeet977@gmail.com>
2019-12-01 16:19:40 -0500

    some changes to applyViscousScaling() for better computation

--- src/navier_stokes_fluxes.hpp
+++ src/navier_stokes_fluxes.hpp
@@ -11,7 +11,7 @@ namespace mach
 /// For constants related to the Navier-Stokes equations
 namespace navierstokes
 {
-/// heat capcity ratio for air
+/// heat capacity ratio for air
 const double gamma = 1.4;
 /// ratio minus one
 const double gami = gamma - 1.0;
@@ -42,38 +42,38 @@ void applyViscousScaling(int i, int j, const xdouble *q, const xdouble *vec,
    e += E;
    T = e / cv;
    // apply diagonal block matrices (Cij; i=j) on `vec`
-   if (i == j)
+  if (i == j)
    {
       // get all entries of `mat_vec` except last one
       for (int k = 0; k < dim; ++k)
       {
-         mat_vec[k + 1] = T * mu * vec[k + 1];
-         mat_vec[k + 1] += T * mu * (q[k + 1] / q[0]) * vec[dim + 1];
+         mat_vec[k + 1] += T * mu * (vec[k + 1] +
+                                    ((q[k + 1] / q[0]) * vec[dim + 1]));
       }
-      mat_vec[i + 1] += (1 / 3) * T * mu * vec[i + 1];
-      mat_vec[i + 1] += (1 / 3) * T * mu * ((q[i + 1] / q[0]) * vec[dim + 1]);
+      mat_vec[i + 1] += (1 / 3) * T * mu * (vec[i + 1] + 
+                                           ((q[i + 1] / q[0]) * vec[dim + 1]));
       // get last entry of `mat_vec`
       for (int p = 0; p < dim; ++p)
       {
-         mat_vec[dim + 1] += T * mu * (q[p + 1] / q[0]) * vec[p + 1];
-         mat_vec[dim + 1] += T * mu * (q[p + 1] / q[0]) * (q[p + 1] / q[0]) * vec[dim + 1];
+         mat_vec[dim + 1] += T * mu * (q[p + 1] / q[0]) * (vec[p + 1] +
+                                             ((q[p + 1] / q[0]) * vec[dim + 1]));
       }
-      mat_vec[dim + 1] += (1 / 3) * T * mu * (q[i + 1] / q[0]) * vec[i + 1];
-      mat_vec[dim + 1] += (1 / 3) * T * mu * (q[i + 1] / q[0]) * (q[i + 1] / q[0]) * vec[dim + 1];
+      mat_vec[dim + 1] += (1 / 3) * T * mu * (q[i + 1] / q[0]) * (vec[i + 1] +
+                                             ((q[i + 1] / q[0]) * vec[dim + 1]));
       mat_vec[dim + 1] += T * T * kappa * vec[dim + 1];
    }
    // apply non-diagonal block matrices (Cij; i!=j) on `vec`
    else
    {
       // get all non-zero entries of `mat_vec` except last 
-      mat_vec[i + 1] = -(2 / 3) * T * mu * vec[j + 1];
-      mat_vec[i + 1] -= (2 / 3) * T * mu * (q[j + 1] / q[0]) * vec[dim + 1];
-      mat_vec[j + 1] = T * mu * vec[i + 1];
-      mat_vec[j + 1] += T * mu * (q[i + 1] / q[0]) * vec[dim + 1];
+      mat_vec[i + 1] -= (2 / 3) * T * mu * (vec[j + 1] +
+                                     ((q[j + 1] / q[0]) * vec[dim + 1]));
+      mat_vec[j + 1] += T * mu * (vec[i + 1] + ((q[i + 1] / q[0]) * vec[dim + 1]));
       // get last entry of `mat_vec`
-      mat_vec[dim + 1] = T * mu * (q[j + 1] / q[0]) * vec[i + 1];
-      mat_vec[dim + 1] -= (2 / 3) * T * mu * (q[i + 1] / q[0]) * vec[j + 1];
-      mat_vec[dim + 1] += (1 / 3) * T * mu * (q[i + 1] / q[0]) * (q[j + 1] / q[0]) * vec[dim + 1];
+      mat_vec[dim + 1]  =(q[j + 1] / q[0]) * vec[i + 1];
+      mat_vec[dim + 1] += (q[i + 1] / q[0]) * ((-(2 / 3) * vec[j + 1]) +
+                                               ((1 / 3) * (q[j + 1] / q[0]) * vec[dim + 1]));
+      mat_vec[dim + 1] *= T * mu;
    }
 }
 
