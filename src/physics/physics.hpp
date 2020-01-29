0601d0f83dadf013b2e2238b78bd8c90d432360a
Tucker Babcock <tbabcock03@gmail.com>
2020-01-29 18:36:01 -0500

    added attempt at making a single header file

---
+++ src/physics/physics.hpp
@@ -0,0 +1,9 @@
+#ifndef MACH_PHYSICS
+#define MACH_PHYSICS
+
+#include "electromagnetics/electromagnetics.hpp"
+#include "fluidflow/fluidflow.hpp"
+#include "diag_mass_integ.hpp"
+#include "solver.hpp"
+
+#endif
