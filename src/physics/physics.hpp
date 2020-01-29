3a86cc2ff90d6e538e2c5e721e3bb2438e6714b7
Tucker Babcock <tbabcock03@gmail.com>
2020-01-29 18:44:44 -0500

    added new headers to cmake files

--- src/physics/physics.hpp
+++ src/physics/physics.hpp
@@ -1,7 +1,7 @@
 #ifndef MACH_PHYSICS
 #define MACH_PHYSICS
 
-#include "electromagnetics/electromagnetics.hpp"
+// #include "electromagnetics/electromagnetics.hpp"
 #include "fluidflow/fluidflow.hpp"
 #include "diag_mass_integ.hpp"
 #include "solver.hpp"
