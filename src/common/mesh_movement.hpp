ac7044f8c5c937d8cd44c92b9bf17f60d9e9d663
Garo Bedonian <bedong@romulus.scorec.rpi.edu>
2020-02-27 14:11:58 -0500

    first commit, added files

---
+++ src/common/mesh_movement.hpp
@@ -0,0 +1,13 @@
+#ifndef MACH_MESH_MOVEMENT
+#define MACH_MESH_MOVEMENT
+
+#include "mfem.hpp"
+#include "adept.h"
+
+#include "solver.hpp"
+#include "coefficient.hpp"
+
+namespace mach
+{
+    
+} //namespace mach
