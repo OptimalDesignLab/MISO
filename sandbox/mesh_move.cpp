8baa29f5c3bf05bec109292a6946eb7e3fd9c948
Garo Bedonian <bedong@romulus.scorec.rpi.edu>
2020-03-04 17:14:35 -0500

    can get displacements, but parameterization is bad

--- sandbox/mesh_move.cpp
+++ sandbox/mesh_move.cpp
@@ -1,5 +1,6 @@
 #include "mfem.hpp"
 #include "thermal.hpp"
+#include "gmi_egads.h"
 
 #include <fstream>
 #include <iostream>
@@ -41,14 +42,18 @@ int main(int argc, char *argv[])
    opts >> file_options;
    options.merge_patch(file_options);
 
+   gmi_register_egads();
+   gmi_egads_start();
+
     apf::Mesh2* pumi_mesh;
     std::unique_ptr<MeshType> mesh;
-
     apf::Mesh2* dummy_mesh;
     gmi_model* new_surf;
-    const char *model_file_old = options["model-file-old"].get<string>().c_str();
-    const char *model_file_new = options["model-file-new"].get<string>().c_str();
-    const char *mesh_file = options["mesh"]["file"].get<string>().c_str();
+    string model_file_old = options["model-file-old"].template get<string>();
+    string model_file_new = options["model-file-new"].template get<string>();
+    string mesh_file = options["mesh"]["file"].template get<string>();
+
+    cout << model_file_old << endl;
     PCU_Comm_Init();
 #ifdef MFEM_USE_SIMMETRIX
     Sim_readLicenseFile(0);
@@ -56,17 +61,21 @@ int main(int argc, char *argv[])
     gmi_register_sim();
 #endif
     gmi_register_mesh();
-    pumi_mesh = apf::loadMdsMesh(model_file_old, mesh_file); 
-        pumi_mesh->verify();
+
+    //new_surf = gmi_load(model_file_new.c_str());
+    //pumi_mesh = apf::loadMdsMesh(model_file_old.c_str(), mesh_file.c_str()); 
+    pumi_mesh = apf::loadMdsMesh(model_file_new.c_str(), mesh_file.c_str()); 
+
+    //    pumi_mesh->verify();
     mesh.reset(new MeshType(MPI_COMM_WORLD, pumi_mesh));
 
     //begin surface mesh operationss
-    new_surf = gmi_load(model_file_new);
+
     apf::MeshEntity* bndn;
     apf::ModelEntity* bnd_face_old;
     apf::ModelEntity* bnd_face_new;
     apf::MeshIterator* itb = pumi_mesh->begin(0);
-    apf::Vector3 param;
+    apf::Vector3 param; apf::Vector3 param2;
     apf::Vector3 xold; apf::Vector3 xnew;
     mfem::Vector disp;
     disp.SetSize(3);
@@ -78,18 +87,26 @@ int main(int argc, char *argv[])
    {
       pumi_mesh->getPoint(bndn, 0, xold);
       pumi_mesh->getParam(bndn, param);
+      cout << "param point: " << param.x() << " "<<  param.y() << " " << param.z() << endl;
+
       bnd_face_old = pumi_mesh->toModel(bndn);
       int tag = pumi_mesh->getModelTag(bnd_face_old);
+      cout << "tag: " << tag << endl;
       int type = pumi_mesh->getModelType(bnd_face_old);
-      bnd_face_new = (apf::ModelEntity*)gmi_find(new_surf, type, tag);
-      gmi_eval(new_surf, (gmi_ent*)bnd_face_new, &param[0], &xnew[0]);
+      cout << "type: " << type << endl;
+      //bnd_face_new = (apf::ModelEntity*)gmi_find(new_surf, type, tag);
+      //gmi_reparam(new_surf, (gmi_ent*)bnd_face_old, &param[0], (gmi_ent*)bnd_face_new, &param2[0]);
+      //gmi_eval(new_surf, (gmi_ent*)bnd_face_new, &param[0], &xnew[0]);
+      gmi_eval(pumi_mesh->getModel(), (gmi_ent*)bnd_face_old, &param[0], &xnew[0]);
       disp(0) = xnew.x() - xold.x();
       disp(1) = xnew.y() - xold.y();
       disp(2) = xnew.z() - xold.z();
       disp_list[j] = disp;
       j++;
+      cout << "node: " << j << ", oldx: " << xold.x() << " " << xold.y() << " " << xold.z() << endl;
+      cout << "node: " << j << ", disp: " << disp(0) << " " << disp(1) << " " << disp(2) << endl;
    }
-
+   gmi_egads_stop();
     PCU_Comm_Free();
 #ifdef MFEM_USE_SIMMETRIX
     gmi_sim_stop();
