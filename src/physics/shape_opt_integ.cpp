137e35536345333e892396f097cbc7a39ae3f825
Garo Bedonian <bedong@romulus.scorec.rpi.edu>
2020-04-14 16:05:50 -0400

    first commit

---
+++ src/physics/shape_opt_integ.cpp
@@ -0,0 +1,1 @@
+#include "shape_opt_integ.hpp"
---
+++ src/physics/shape_opt_integ.hpp
@@ -0,0 +1,9 @@
+#ifndef MACH_EULER_INTEG
+#define MACH_EULER_INTEG
+
+#include "adept.h"
+#include "mfem.hpp"
+
+/// Commonly-used integrators adapted to compute gradients with respect to dof coordinates
+
+#endif
