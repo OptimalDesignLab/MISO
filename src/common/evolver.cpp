1092f152f6b2c948d6cca164cc89a6ef3addc3ac
Jason Hicken <hickej2@rpi.edu>
2020-05-16 11:22:54 -0400

    added EntropyConstrainedOperator and RRKImplicitMidpoint

--- src/common/evolver.cpp
+++ src/common/evolver.cpp
@@ -7,6 +7,39 @@ using namespace std;
 namespace mach
 {
 
+void RRKImplicitMidpointSolver::Init(TimeDependentOperator &_f)
+{
+   ODESolver::Init(_f);
+   k.SetSize(f->Width(), mem_type);
+}
+
+void RRKImplicitMidpointSolver::Step(Vector &x, double &t, double &dt)
+{
+   f->SetTime(t + dt/2);
+   f->ImplicitSolve(dt/2, x, k);
+
+   // Set-up and solve the scalar nonlinear problem for the relaxation gamma
+   EntropyConstrainedOperator *f_ode =
+       dynamic_cast<EntropyConstrainedOperator *>(f);
+   double entropy_old = f_ode->Entropy(x);
+   double delta_entropy = f_ode->EntropyChange(dt/2, x, k);
+   mfem::Vector x_new(x.Size());
+   auto entropyFun = [&](double gamma)
+   {
+      add(x, gamma*dt, k, x_new);
+      double entropy = f_ode->Entropy(x_new);
+      return entropy - entropy_old + gamma*dt*delta_entropy;
+   };
+   // TODO: tolerances and maxiter should be provided in some other way
+   const double ftol = 1e-12;
+   const double xtol = 1e-12;
+   const int maxiter = 30;
+   double gamma = secant(entropyFun, 0.99, 1.01, ftol, xtol, maxiter);
+   // cout << "\tgamma = " << gamma << endl;
+   x.Add(gamma*dt, k);
+   t += gamma*dt;
+}
+
 LinearEvolver::LinearEvolver(MatrixType &m, MatrixType &k, ostream &outstream)
    : out(outstream), TimeDependentOperator(m.Height()), mass(m), stiff(k), z(m.Height())
 {
