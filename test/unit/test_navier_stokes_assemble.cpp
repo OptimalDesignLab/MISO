3fa4dbfbfa64cea8998ca10ec8a18840ec2eb41f
Jason Hicken <hickej2@rpi.edu>
2020-07-07 18:44:02 -0400

    added test for SurfaceForce::AssembleElementVector

--- test/unit/test_navier_stokes_assemble.cpp
+++ test/unit/test_navier_stokes_assemble.cpp
@@ -414,3 +414,110 @@ TEST_CASE("ViscousFarFieldBC::AssembleFaceGrad", "[VisFarFieldBC]")
       }
    }
 }
+
+TEST_CASE("SurfaceForce::AssembleVector", "[Surface Force]")
+{
+   using namespace mfem;
+   using namespace euler_data;
+
+   const int dim = 2; // templating is hard here because mesh constructors
+   int num_state = dim + 2;
+   adept::Stack diff_stack;
+   double delta = 1e-5;
+
+   mfem::Vector drag_dir(dim);
+   double aoa_fs = 5.0*M_PI/180;
+   double Re = 1.0;
+   double Pr = 0.72;
+   drag_dir(0) = cos(aoa_fs);
+   drag_dir(1) = sin(aoa_fs);
+
+   mfem::Vector q_ref(dim + 2);
+   q_ref(0) = rho;
+   q_ref(dim + 1) = rhoe;
+   for (int di = 0; di < dim; ++di)
+   {
+      q_ref(di + 1) = rhou[di];
+   }
+
+   // generate a 2 element mesh
+   int num_edge = 2;
+   std::unique_ptr<Mesh> mesh(new Mesh(num_edge, num_edge, Element::TRIANGLE,
+                                       true /* gen. edges */, 1.0, 1.0, true));
+   for (int p = 1; p <= 4; ++p)
+   {
+      DYNAMIC_SECTION("...for degree p = " << p)
+      {
+         std::unique_ptr<FiniteElementCollection> fec(
+             new SBPCollection(p, dim));
+         std::unique_ptr<FiniteElementSpace> fes(new FiniteElementSpace(
+             mesh.get(), fec.get(), num_state, Ordering::byVDIM));
+
+         NonlinearForm drag(fes.get());
+         drag.AddBdrFaceIntegrator(
+             new mach::SurfaceForce<dim>(diff_stack, fec.get(), dim + 2, Re, Pr,
+                                         q_ref, drag_dir));
+
+         // initialize state; here we randomly perturb a constant state
+         GridFunction q(fes.get());
+         VectorFunctionCoefficient pert(num_state, randBaselinePert<dim>);
+         q.ProjectCoefficient(pert);
+
+         // initialize the vector that dJdu multiplies
+         GridFunction v(fes.get());
+         VectorFunctionCoefficient v_rand(num_state, randState);
+         v.ProjectCoefficient(v_rand);
+
+         // evaluate dJdu and compute its product with v
+         GridFunction dJdu(fes.get());
+         drag.Mult(q, dJdu);
+         double dJdu_dot_v = InnerProduct(dJdu, v);
+
+         // now compute the finite-difference approximation...
+         GridFunction q_pert(q);
+         q_pert.Add(-delta, v);
+         double dJdu_dot_v_fd = -drag.GetEnergy(q_pert);
+         q_pert.Add(2 * delta, v);
+         dJdu_dot_v_fd += drag.GetEnergy(q_pert);
+         dJdu_dot_v_fd /= (2 * delta);
+         REQUIRE(dJdu_dot_v == Approx(dJdu_dot_v_fd).margin(1e-10));
+      }
+
+      DYNAMIC_SECTION("(DSBP)...for degree p = " << p)
+      {
+         std::unique_ptr<FiniteElementCollection> fec(
+             new DSBPCollection(p, dim));
+         std::unique_ptr<FiniteElementSpace> fes(new FiniteElementSpace(
+             mesh.get(), fec.get(), num_state, Ordering::byVDIM));
+
+         NonlinearForm drag(fes.get());
+         drag.AddBdrFaceIntegrator(
+             new mach::SurfaceForce<dim>(diff_stack, fec.get(), dim + 2, Re, Pr,
+                                         q_ref, drag_dir));
+
+         // initialize state; here we randomly perturb a constant state
+         GridFunction q(fes.get());
+         VectorFunctionCoefficient pert(num_state, randBaselinePert<dim>);
+         q.ProjectCoefficient(pert);
+
+         // initialize the vector that dJdu multiplies
+         GridFunction v(fes.get());
+         VectorFunctionCoefficient v_rand(num_state, randState);
+         v.ProjectCoefficient(v_rand);
+
+         // evaluate dJdu and compute its product with v
+         GridFunction dJdu(fes.get());
+         drag.Mult(q, dJdu);
+         double dJdu_dot_v = InnerProduct(dJdu, v);
+
+         // now compute the finite-difference approximation...
+         GridFunction q_pert(q);
+         q_pert.Add(-delta, v);
+         double dJdu_dot_v_fd = -drag.GetEnergy(q_pert);
+         q_pert.Add(2 * delta, v);
+         dJdu_dot_v_fd += drag.GetEnergy(q_pert);
+         dJdu_dot_v_fd /= (2 * delta);
+         REQUIRE(dJdu_dot_v == Approx(dJdu_dot_v_fd).margin(1e-10));
+      }
+   }
+}
