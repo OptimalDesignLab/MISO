5cba38286e887c6a25294d4c1dea494a9c3d8213
Tucker Babcock <tbabcock03@gmail.com>
2020-11-17 22:39:04 -0500

    adding hyperco50 and team13 materials to the material library

--- src/common/material_library.hpp
+++ src/common/material_library.hpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9df8ddb8e67ad257c1d9b54123758b1f864a9658f8a005abff74e29c221b8218
-size 5950
+oid sha256:bcb0d7ac76dbad41d62f81bbe5f07f71e363c1e91700ca92dbcfba3c800b9cb4
+size 10929
