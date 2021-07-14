24270a98bcd9a271768801bfe100c511d47c6dda
Tucker Babcock <tbabcock03@gmail.com>
2021-07-14 14:24:02 -0400

    cleaned up old force integrator

--- src/physics/electromagnetics/electromag_integ.cpp
+++ src/physics/electromagnetics/electromag_integ.cpp
@@ -2581,10 +2581,10 @@ double ForceIntegrator::GetElementEnergy(
    ElementTransformation &trans,
    const Vector &elfun)
 {
-   // if (attrs.count(trans.Attribute) == 1)
-   // {
-   //    return 0.0;
-   // }
+   if (attrs.count(trans.Attribute) == 1)
+   {
+      return 0.0;
+   }
    /// get the proper element, transformation, and v vector
    Array<int> vdofs; Vector vfun; 
    int element = trans.ElementNo;
@@ -2592,10 +2592,10 @@ double ForceIntegrator::GetElementEnergy(
    v.FESpace()->GetElementVDofs(element, vdofs);
    v.GetSubVector(vdofs, vfun);
    DenseMatrix dXds(vfun.GetData(), v_el.GetDof(), v_el.GetDim());
-   // if (vfun.Normlinf() < 1e-14)
-   // {
-   //    return 0.0;
-   // }
+   if (vfun.Normlinf() < 1e-14)
+   {
+      return 0.0;
+   }
    /// number of degrees of freedom
    int ndof = el.GetDof();
    int dim = el.GetDim();
@@ -2693,182 +2693,9 @@ double ForceIntegrator::GetElementEnergy(
       Mult(trans.InverseJacobian(), dJds, JinvdJds);
 
       force += energy * JinvdJds.Trace();
-
-      if (!std::isfinite(force))
-      {
-         std::cout << "nonfinite!\n";
-      }
       fun -= force * w;
    }
    return fun;
 }
-
-// ForceIntegrator::ForceIntegrator(AbstractSolver *_solver,
-//                                  std::unordered_set<int> _regions,
-//                                  std::unordered_set<int> _free_regions,
-//                                  StateCoefficient *_nu,
-//                                  Vector _dir)
-//    : solver(_solver), regions(_regions), free_regions(_free_regions), nu(_nu),
-//      dir(_dir)
-// {
-// #ifndef MFEM_USE_PUMI
-//    throw MachException("ForceIntegrator::ForceIntegrator()\n"
-//                         "\tusing ForceIntegrator requires PUMI!\n");
-// }
-
-// double ForceIntegrator::GetElementEnergy(const FiniteElement &el,
-//                                          ElementTransformation &trans,
-//                                          const Vector &elfun)
-// {
-//    throw MachException("ForceIntegrator::ForceIntegrator()\n"
-//                         "\tusing ForceIntegrator requires PUMI!\n");
-// }
-// #else
-//    /// TODO: Call pumi APIs to get a list of mesh face indices that are on the
-//    ///       boundary of the regions given in regions
-
-//    // std::unordered_set<int> face_list;
-
-//    apf::Mesh2 *pumi_mesh = solver->getPumiMesh();
-//    /// get the underlying gmi model
-//    auto *model = pumi_mesh->getModel();
-
-//    /// find the model faces that define the interface between moving and fixed
-//    /// parts
-//    for (auto &free_region_tag : free_regions)
-//    {
-//       auto *free_region = gmi_find(model, 3, free_region_tag);
-//       auto *adjacent_faces = gmi_adjacent(model, free_region, 2);
-//       for (int i = 0; i < adjacent_faces->n; ++i)
-//       {
-//          auto adjacent_face = adjacent_faces->e[i];
-//          for (auto &moving_region_tag : regions)
-//          {
-//             auto *moving_region = gmi_find(model, 3, moving_region_tag);
-//             if (gmi_is_in_closure_of(model, adjacent_face, moving_region))
-//             {
-//                int face_tag = gmi_tag(model, adjacent_face);
-//                face_list.insert(face_tag);
-//             }
-//          }
-//       }
-//       gmi_free_set(adjacent_faces);
-//    }
-
-//    /// loop over all mesh faces
-//    apf::MeshEntity *e;
-//    apf::MeshIterator* ent_it = pumi_mesh->begin(2);
-//    while ((e = pumi_mesh->iterate(ent_it)))
-//    {
-//       int e_tag = gmi_tag(model, (gmi_ent*)pumi_mesh->toModel(e));
-//       auto search = face_list.find(e_tag);
-//       if (search != face_list.end())
-//       {
-//          auto r_1 = pumi_mesh->getUpward(e,0);
-//          int r_1_tag = gmi_tag(model, (gmi_ent*)pumi_mesh->toModel(r_1));
-//          auto search_tet = free_regions.find(r_1_tag);
-//          if (search_tet != free_regions.end())
-//          {
-//             el_ids.insert(apf::getMdsIndex(pumi_mesh, r_1));
-//          }
-//          else
-//          {
-//             auto r_2 = pumi_mesh->getUpward(e,1);
-//             el_ids.insert(apf::getMdsIndex(pumi_mesh, r_2));
-//          }
-//       }
-//    }
-//    pumi_mesh->end(ent_it);
-// }
-
-// double ForceIntegrator::GetElementEnergy(const FiniteElement &el,
-//                                          ElementTransformation &trans,
-//                                          const Vector &elfun)
-// {
-//    /// number of degrees of freedom
-//    int ndof = el.GetDof();
-//    int dim = el.GetDim();
-
-//    /// I believe this takes advantage of a 2D problem not having
-//    /// a properly defined curl? Need more investigation
-//    int dimc = (dim == 3) ? 3 : 1;
-
-//    /// holds quadrature weight
-//    double w;
-
-// #ifdef MFEM_THREAD_SAFE
-//    DenseMatrix curlshape(ndof,dimc), curlshape_dFt(ndof,dimc), M;
-//    Vector b_vec(dimc);
-// #else
-//    curlshape.SetSize(ndof,dimc);
-//    curlshape_dFt.SetSize(ndof,dimc);
-//    b_vec.SetSize(dimc);
-// #endif
-
-//    const IntegrationRule *ir = IntRule;
-//    if (ir == NULL)
-//    {
-//       int order;
-//       if (el.Space() == FunctionSpace::Pk)
-//       {
-//          order = 2*el.GetOrder() - 2;
-//       }
-//       else
-//       {
-//          order = 2*el.GetOrder();
-//       }
-
-//       ir = &IntRules.Get(el.GetGeomType(), order);
-//    }
-
-//    double fun = 0.0;
-
-//    for (int i = 0; i < ir->GetNPoints(); i++)
-//    {
-//       b_vec = 0.0;
-//       const IntegrationPoint &ip = ir->IntPoint(i);
-
-//       trans.SetIntPoint(&ip);
-
-//       w = ip.weight / trans.Weight();
-
-//       if ( dim == 3 )
-//       {
-//          el.CalcCurlShape(ip, curlshape);
-//          MultABt(curlshape, trans.Jacobian(), curlshape_dFt);
-//       }
-//       else
-//       {
-//          el.CalcCurlShape(ip, curlshape_dFt);
-//       }
-
-//       curlshape_dFt.AddMultTranspose(elfun, b_vec);
-//       double model_val = nu->Eval(trans, ip, b_vec.Norml2());
-//       model_val *= w;
-
-//       double el_en = b_vec*b_vec;
-//       el_en *= 0.5 * model_val;
-
-//       fun += el_en;
-//    }
-//    return fun;
-// }
-// #endif
-
-// double ForceIntegrator::GetFaceEnergy(const FiniteElement &el1,
-//                                       const FiniteElement &el2,
-//                                       FaceElementTransformations &Tr,
-//                                       const Vector &elfun)
-// {
-
-// }
-
-// double VWTorqueIntegrator::GetElementEnergy(const FiniteElement &el,
-//                                             ElementTransformation &Tr,
-//                                             const Vector &elfun)
-// {
-
-//    return 0.0;
-// }
 
 } // namespace mach
