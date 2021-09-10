53e64737c33ec605413ddb7bf09d7005c5d9b3a8
Tucker Babcock <tbabcock03@gmail.com>
2021-09-10 10:47:31 -0600

    removing unused relaxed newton solver

--- src/common/mfem_extensions.hpp
+++ src/common/mfem_extensions.hpp
@@ -38,36 +38,6 @@ protected:
    std::ostream *out;
 };
 
-class RelaxedNewton: public mfem::NewtonSolver
-{
-public:
-   RelaxedNewton(MPI_Comm comm)
-   :  mfem::NewtonSolver(comm), first_iter(true)
-   { }
-
-   void SetOperator(const mfem::Operator &op) override;
-
-   /// Solve the nonlinear system with right-hand side @a b.
-   /** If `b.Size() != Height()`, then @a b is assumed to be zero. */
-   void Mult(const mfem::Vector &b, mfem::Vector &x) const override;
-
-   double ComputeScalingFactor(const mfem::Vector &x,
-                               const mfem::Vector &b) const override;
-
-   /// Set the operator used to calculate the energy for the line search
-   void SetEnergyOperator(const mfem::NonlinearForm &op);
-
-   void SetLoad(const mfem::Vector *_load) {load = _load;};
-
-protected:
-   mutable mfem::Vector rkp1, xkp1;
-
-   const mfem::NonlinearForm *energy;
-   const mfem::Vector *load;
-
-   mutable bool first_iter;
-};
-
 } // namespace mach
 
 #endif
