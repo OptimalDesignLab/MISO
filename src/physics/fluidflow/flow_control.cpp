202bd531b8b3a5c0d4a42f2d251466e0d63ca87d
Jason Hicken <jason.hicken@gmail.com>
2021-10-26 09:20:29 -0400

    added ODE residual class for flow-control problem

--- src/physics/fluidflow/flow_control.cpp
+++ src/physics/fluidflow/flow_control.cpp
@@ -11,20 +11,60 @@ using namespace mfem;
 namespace mach 
 {
 
+void setInputs(PassiveControlResidual &residual, const MachInputs &inputs)
+{
+   setValueFromInputs(inputs, "Kp", residual.Kp);
+   setValueFromInputs(inputs, "Td", residual.Td);
+   setValueFromInputs(inputs, "Ti", residual.Ti);
+   setValueFromInputs(inputs, "alpha", residual.alpha);
+   setValueFromInputs(inputs, "beta", residual.beta);
+   setValueFromInputs(inputs, "entropy_targ", residual.entropy_targ);
+   setFieldFromInputs(inputs, "state", residual.x.begin());
+}
+
+void setOptions(PassiveControlResidual &residual, const nlohmann::json &options)
+{
+   // set options here 
+}
+
+void evaluate(PassiveControlResidual &residual, const MachInputs &inputs, 
+              mfem::Vector &res_vec)
+{
+   // define some aliases 
+   double &Kp = residual.Kp;
+   double &Td = residual.Td;
+   double &Ti = residual.Ti;
+   double &alpha = residual.alpha;
+   double &beta = residual.beta;
+   double &entropy_targ = residual.entropy_targ;
+   // extract the control variables ("state") and entropy from inputs
+   const bool error_if_not_found = true;
+   setFieldFromInputs(inputs, "state", residual.x.begin(), error_if_not_found);
+   double entropy;
+   setValueFromInputs(inputs, "entropy", entropy, error_if_not_found);
+   // evaluate the residual
+   double e = entropy_targ - entropy;
+   res_vec(0) = -((1.0 - 1.0/(alpha*beta))*Kp*e - x(1)/beta)/(alpha*Ti*Td);
+   res_vec(1) = -( ((Td + Ti) - (alpha*Td + beta*Ti)/(alpha*beta))*Kp*e / 
+                   (alpha*Ti*Td) + x(0) - x(1)*(alpha*Td + beta*Ti) /
+                   (alpha*beta*Ti*Td) );
+}
+
 FlowControlResidual::FlowControlResidual(
     mfem::ParFiniteElementSpace &pfes,
     std::unordered_map<std::string, mfem::ParGridFunction> &fields,
     const nlohmann::json &flow_options, 
     const nlohmann::json &space_dis_options,
-    const nlohmann::json &control_options) : flow_res(pfes, fields)
+    const nlohmann::json &control_options) 
+    : flow_res(pfes, fields), control_res(control_options)
 {
    addFlowResidualIntegrators(flow_res, flow_options, space_dis_options);
-   num_control = control_options["num-control"];
+   // TODO: need to add integrators for entropy output
 }
 
 int getSize(const FlowControlResidual &residual) 
 {
-   return getSize(residual.flow_res) + num_control;
+   return getSize(residual.flow_res) + getSize(residual.control_res);
 }
 
 void setInputs(FlowControlResidual &residual, const MachInputs &inputs)
@@ -40,23 +80,31 @@ void setOptions(FlowControlResidual &residual, const nlohmann::json &options)
 void evaluate(FlowControlResidual &residual, const MachInputs &inputs, 
               mfem::Vector &res_vec)
 {
-   Vector control_res_vec(res_vec.GetData() + 0, num_control);
+   Vector control_res_vec(res_vec.GetData() + 0, getSize(residual.control_res));
    Vector flow_res_vec(res_vec.GetData() + num_control,
                        getSize(residual.flow_res));
-   double time = inputs.at("time");
+
+   // Compute the outputs of the flow and control problems needed by the other
+   double time = inputs.at("time").getValue();
    double * control_state_ptr = inputs.at("state").getField();
    double * flow_state_ptr = inputs.at("state").getField() + num_control;
-   // set inputs for flow residual and evaluate it 
    auto flow_inputs = MachInputs({
-      {"state", flow_state_ptr}, {"time", time}, {"control", control_state_ptr}
+      {"state", flow_state_ptr}, {"time", time}
    });
+   double entropy = calcOutput(residual.boundary_entropy, flow_inputs);
+   auto control_inputs = MachInputs({
+      {"state", control_state_ptr}
+   });
+   double velocity = calcOutput(residual.velocity, control_inputs);
+
+   // Evaluate the flow residual 
+   flow_inputs.emplace("input-velocity", velocity);
    // WARNING: as of writing, evaluate does not call setInputs on itself
    evaluate(residual.flow_res, flow_inputs, flow_res_vec);
 
-   // set inputs for ODE residual and evaluate it 
-   // probably need a MachOutput to compute flow value need by ODE
-
-
+   // Evaluate the control residual 
+   control_inputs.emplace("entropy", entropy);
+   evaluate(residual.control_res, control_inputs, control_res_vec);
 }
 
 Operator &getJacobian(FlowControlResidual &residual, const MachInputs &inputs,
--- src/physics/fluidflow/flow_control.hpp
+++ src/physics/fluidflow/flow_control.hpp
@@ -5,6 +5,84 @@
 #include "nlohmann/json.hpp"
 #include "mach_nonlinearform.hpp"
 
+/// Defines the ODE for passive system control-law
+class PassiveControlResidual final 
+{
+   /// Gets the number of control ODEs/unknwons
+   /// \param[inout] residual - the residual whose size is being queried
+   /// \returns the number of equations/unknowns
+   friend int getSize(const PassiveControlResidual &residual)
+   {
+      return residual.num_var;
+   }
+
+   /// Set inputs in the underlying passive-control residual
+   /// \param[inout] residual - passive-control residual being assigned inputs
+   /// \param[in] inputs - the inputs that are being assigned
+   friend void setInputs(PassiveControlResidual &residual,
+                         const MachInputs &inputs);
+
+   /// Set options in the passive-control residual
+   /// \param[inout] residual - passive-control residual whose options are set
+   /// \param[in] options - the options that are being assigned
+   friend void setOptions(PassiveControlResidual &residual,
+                          const nlohmann::json &options);
+
+   /// Evaluate the passive-control residual using inputs and return `res_vec`
+   /// \param[inout] residual - passive-control residual being evaluated
+   /// \param[in] inputs - the independent variables at which to evaluate res
+   /// \param[out] res_vec - the dependent variable, the output from `residual`
+   friend void evaluate(PassiveControlResidual &residual,
+                        const MachInputs &inputs,
+                        mfem::Vector &res_vec);
+
+   /// Compute the "Jacobian" of the passive-control residual and return it
+   /// \param[inout] residual - passive-control residual whose Jacobian we want
+   /// \param[in] inputs - the variables needed to evaluate the Jacobian
+   /// \param[in] wrt - the input we are differentiating with respect to
+   /// \returns a reference to the residual's Jacobian with respect to `wrt`
+   /// \note the underlying `Operator` is owned by `residual`
+   friend mfem::Operator &getJacobian(PassiveControlResidual &residual,
+                                      const MachInputs &inputs,
+                                      std::string wrt);
+
+   /// Evaluate the entropy/Lyapunov functional at the given state
+   /// \param[inout] residual - passive-control residual whose entropy we want
+   /// \param[in] inputs - the variables needed to evaluate the entropy
+   /// \return the entropy functional
+   /// \note optional, but must be implemented for relaxation RK
+   friend double calcEntropy(PassiveControlResidual &residual,
+                             const MachInputs &inputs);
+
+   /// Evaluate the passive-control residual weighted by the entropy variables
+   /// \param[inout] residual - function with an associated entropy
+   /// \param[in] inputs - the variables needed to evaluate the entropy
+   /// \return the product `w^T res`
+   /// \note `w` and `res` are evaluated at `state + dt*state_dot` and time
+   /// `t+dt` as provided by `inputs`.
+   /// \note optional, but must be implemented for relaxation RK
+   friend double calcEntropyChange(PassiveControlResidual &residual,
+                                   const MachInputs &inputs);
+
+   /// Constructor 
+   /// \param[in] control_options - options used to define the residual
+   /// \note the number of control variables is hard-coded, but this could 
+   /// easily be changed.
+   PassiveControlResidual(const nlohmann::json &control_options)
+    : num_var(2), x(2), Jac(2) { }
+
+private:
+   /// number of control ODE variables/equations
+   int num_var;
+   /// parameters in the control law
+   double Kp, Td, Ti, alpha, beta;
+   /// desired, or target, entropy from flow
+   double entropy_targ;
+   /// work vector to store the "state" (i.e. the control variables)
+   mfem::Vector x;
+   /// Jacobian of the ODE right-hand side
+   mfem::DenseMatrix Jac;
+};
 
 class FlowControlResidual final
 {
@@ -72,7 +150,8 @@ public:
 private:
    /// Defines the CFD discretization of the problem
    MachNonlinearForm flow_res;
-
+   PassiveControlResidual control_res;
+   FunctionalOutput boundary_entropy;
 
 
 
