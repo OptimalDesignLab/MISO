51579ba9e172029aeb7750eaee65125054755e98
Jason Hicken <jason.hicken@gmail.com>
2021-12-08 08:41:31 -0500

    renamed FluidResidual to FlowResidual, as well as files

--- src/physics/fluidflow/flow_residual.cpp
+++ src/physics/fluidflow/flow_residual.cpp
@@ -1,6 +1,6 @@
 #include "mfem.hpp"
 
-#include "fluid_residual.hpp"
+#include "flow_residual.hpp"
 #include "euler_fluxes.hpp"
 #include "euler_integ.hpp"
 
@@ -9,7 +9,7 @@ using namespace mfem;
 
 namespace mach
 {
-FluidResidual::FluidResidual(const nlohmann::json &options,
+FlowResidual::FlowResidual(const nlohmann::json &options,
                              ParFiniteElementSpace &fespace,
                              adept::Stack &diff_stack)
  : fes(fespace),
@@ -25,27 +25,27 @@ FluidResidual::FluidResidual(const nlohmann::json &options,
    switch (dim)
    {
    case 1:
-      addFluidIntegrators<1>(options);
+      addFlowIntegrators<1>(options);
       break;
    case 2:
-      addFluidIntegrators<2>(options);
+      addFlowIntegrators<2>(options);
       break;
    case 3:
-      addFluidIntegrators<3>(options);
+      addFlowIntegrators<3>(options);
       break;
    default:
-      throw MachException("Invalid space dimension for FluidResidual!\n");
+      throw MachException("Invalid space dimension for FlowResidual!\n");
    }
 }
 
 template <int dim>
-void FluidResidual::addFluidIntegrators(const nlohmann::json &options)
+void FlowResidual::addFlowIntegrators(const nlohmann::json &options)
 {
    if (!options.contains("flow-param") || !options.contains("space-dis") ||
        !options.contains("bcs"))
    {
       throw MachException(
-          "FluidResidual::addFluidIntegrators: options must"
+          "FlowResidual::addFlowIntegrators: options must"
           "contain flow-param, space-dis, and bcs!\n");
    }
    nlohmann::json flow = options["flow-param"];
@@ -54,23 +54,23 @@ void FluidResidual::addFluidIntegrators(const nlohmann::json &options)
    if (state_is_entvar)
    {
       // Use entropy variables for the state
-      addFluidDomainIntegrators<dim, true>(flow, space_dis);
-      addFluidInterfaceIntegrators<dim, true>(flow, space_dis);
-      addFluidBoundaryIntegrators<dim, true>(flow, space_dis, bcs);
+      addFlowDomainIntegrators<dim, true>(flow, space_dis);
+      addFlowInterfaceIntegrators<dim, true>(flow, space_dis);
+      addFlowBoundaryIntegrators<dim, true>(flow, space_dis, bcs);
       addEntropyIntegrators<dim, true>();
    }
    else
    {
       // Use the conservative variables for the state
-      addFluidDomainIntegrators<dim>(flow, space_dis);
-      addFluidInterfaceIntegrators<dim>(flow, space_dis);
-      addFluidBoundaryIntegrators<dim>(flow, space_dis, bcs);
+      addFlowDomainIntegrators<dim>(flow, space_dis);
+      addFlowInterfaceIntegrators<dim>(flow, space_dis);
+      addFlowBoundaryIntegrators<dim>(flow, space_dis, bcs);
       addEntropyIntegrators<dim>();
    }
 }
 
 template <int dim, bool entvar>
-void FluidResidual::addFluidDomainIntegrators(const nlohmann::json &flow,
+void FlowResidual::addFlowDomainIntegrators(const nlohmann::json &flow,
                                               const nlohmann::json &space_dis)
 {
    if (space_dis["flux-fun"].get<string>() == "IR")
@@ -97,7 +97,7 @@ void FluidResidual::addFluidDomainIntegrators(const nlohmann::json &flow,
 }
 
 template <int dim, bool entvar>
-void FluidResidual::addFluidInterfaceIntegrators(
+void FlowResidual::addFlowInterfaceIntegrators(
     const nlohmann::json &flow,
     const nlohmann::json &space_dis)
 {
@@ -111,7 +111,7 @@ void FluidResidual::addFluidInterfaceIntegrators(
 }
 
 template <int dim, bool entvar>
-void FluidResidual::addFluidBoundaryIntegrators(const nlohmann::json &flow,
+void FlowResidual::addFlowBoundaryIntegrators(const nlohmann::json &flow,
                                                 const nlohmann::json &space_dis,
                                                 const nlohmann::json &bcs)
 {
@@ -120,7 +120,7 @@ void FluidResidual::addFluidBoundaryIntegrators(const nlohmann::json &flow,
       if (dim != 2)
       {
          throw MachException(
-             "FluidResidual::addFluidBoundaryIntegrators()\n"
+             "FlowResidual::addFlowBoundaryIntegrators()\n"
              "\tisentropic vortex BC must use 2D mesh!");
       }
       vector<int> bdr_attr_marker = bcs["vortex"].get<vector<int>>();
@@ -148,20 +148,20 @@ void FluidResidual::addFluidBoundaryIntegrators(const nlohmann::json &flow,
 }
 
 template <int dim, bool entvar>
-void FluidResidual::addEntropyIntegrators()
+void FlowResidual::addEntropyIntegrators()
 {
    ent.addOutputDomainIntegrator(new EntropyIntegrator<dim, entvar>(stack));
 }
 
-int getSize(const FluidResidual &residual) { return getSize(residual.res); }
+int getSize(const FlowResidual &residual) { return getSize(residual.res); }
 
-void setInputs(FluidResidual &residual, const MachInputs &inputs)
+void setInputs(FlowResidual &residual, const MachInputs &inputs)
 {
    // What if aoa_fs or mach_fs are being changed?
    setInputs(residual.res, inputs);
 }
 
-void setOptions(FluidResidual &residual, const nlohmann::json &options)
+void setOptions(FlowResidual &residual, const nlohmann::json &options)
 {
    residual.is_implicit = options.value("implicit", false);
    // define free-stream parameters; may or may not be used, depending on case
@@ -185,26 +185,26 @@ void setOptions(FluidResidual &residual, const nlohmann::json &options)
    setOptions(residual.res, options);
 }
 
-void evaluate(FluidResidual &residual,
+void evaluate(FlowResidual &residual,
               const MachInputs &inputs,
               Vector &res_vec)
 {
    evaluate(residual.res, inputs, res_vec);
 }
 
-mfem::Operator &getJacobian(FluidResidual &residual,
+mfem::Operator &getJacobian(FlowResidual &residual,
                             const MachInputs &inputs,
                             const string &wrt)
 {
    return getJacobian(residual.res, inputs, wrt);
 }
 
-double calcEntropy(FluidResidual &residual, const MachInputs &inputs)
+double calcEntropy(FlowResidual &residual, const MachInputs &inputs)
 {
    return calcOutput(residual.ent, inputs);
 }
 
-double calcEntropyChange(FluidResidual &residual, const MachInputs &inputs)
+double calcEntropyChange(FlowResidual &residual, const MachInputs &inputs)
 {
    Vector x;
    setVectorFromInputs(inputs, "state", x, false, true);
