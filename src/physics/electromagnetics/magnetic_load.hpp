5676ebfddec8b678835125e8fe2b49ebcb3dc83b
Tucker Babcock <tbabcock03@gmail.com>
2022-02-16 17:30:45 -0700

    removing obsolete LecacyMagneticLoad class and cleaning up test

--- src/physics/electromagnetics/magnetic_load.hpp
+++ src/physics/electromagnetics/magnetic_load.hpp
@@ -64,53 +64,6 @@ private:
    std::unique_ptr<mfem::ScalarVectorProductCoefficient> nuM;
 };
 
-class LegacyMagneticLoad final
-{
-public:
-   /// Used to set scalar inputs in the underlying load type
-   /// Ends up calling `setInputs` on either the `MachLinearForm` or
-   /// a specialized version for each particular load.
-   friend void setInputs(LegacyMagneticLoad &load, const MachInputs &inputs);
-
-   friend inline void setOptions(LegacyMagneticLoad &load,
-                                 const nlohmann::json &options)
-   { }
-
-   /// Assemble the load vector on the true dofs and store in tv
-   friend void addLoad(LegacyMagneticLoad &load, mfem::Vector &tv);
-
-   friend double vectorJacobianProduct(LegacyMagneticLoad &load,
-                                       const mfem::Vector &res_bar,
-                                       const std::string &wrt);
-
-   friend void vectorJacobianProduct(LegacyMagneticLoad &load,
-                                     const mfem::Vector &res_bar,
-                                     const std::string &wrt,
-                                     mfem::Vector &wrt_bar);
-
-   LegacyMagneticLoad(mfem::ParFiniteElementSpace &pfes,
-                      mfem::VectorCoefficient &mag_coeff,
-                      mfem::Coefficient &nu);
-
-private:
-   mfem::ParFiniteElementSpace &fes;
-   mfem::RT_FECollection rt_coll;
-   mfem::ParFiniteElementSpace rt_fes;
-
-   mfem::VectorCoefficient &mag_coeff;
-   mfem::Vector load;
-
-   mfem::ParMixedBilinearForm weakCurlMuInv;
-   mfem::ParGridFunction M;
-   mfem::ParGridFunction scratch;
-
-   /// flag to know if the load vector should be reassembled
-   bool dirty;
-
-   /// Assemble the magnetic source load vector
-   void assembleLoad();
-};
-
 }  // namespace mach
 
 #endif
--- test/unit/test_magnetic_load.cpp
+++ test/unit/test_magnetic_load.cpp
@@ -21,63 +21,6 @@ using namespace mach;
 /// \param[in] nz - number of nodes in the z direction
 mfem::Mesh buildMesh(int nxy, int nz);
 
-// TEST_CASE("LegacyMagneticLoad Value Test")
-// {
-
-//    Mesh smesh = buildMesh(10, 10);
-//    ParMesh mesh(MPI_COMM_WORLD, smesh);
-//    mesh.EnsureNodes();
-
-//    auto p = 2;
-//    const auto dim = mesh.Dimension();
-
-//    // get the finite-element space for the state
-//    ND_FECollection fec(p, dim);
-//    ParFiniteElementSpace fes(&mesh, &fec);
-
-//    // create mag_coeff coefficient
-//    VectorMeshDependentCoefficient mag_coeff(dim);
-//    {
-//       std::unique_ptr<mfem::VectorCoefficient> temp_coeff(
-//             new VectorFunctionCoefficient(dim,
-//                                           northMagnetizationSource));
-//       mag_coeff.addCoefficient(1, move(temp_coeff));
-//    }
-//    {
-//       std::unique_ptr<mfem::VectorCoefficient> temp_coeff(
-//             new VectorFunctionCoefficient(dim,
-//                                           southMagnetizationSource));
-//       mag_coeff.addCoefficient(2, move(temp_coeff));
-//    }
-
-//    // create nu coeff
-//    ConstantCoefficient nu(1.0);///(M_PI*4e-7));
-
-//    LegacyMagneticLoad load(fes, mag_coeff, nu);
-//    MachLoad ml(load);
-
-//    HypreParVector tv(&fes);
-
-//    MachInputs inputs;
-//    setInputs(ml, inputs);
-//    tv = 0.0;
-//    addLoad(ml, tv);
-
-//    auto norm = ParNormlp(tv, 2.0, MPI_COMM_WORLD);
-//    // std::cout << "norm: " << norm << "\n";
-
-//    REQUIRE(norm == Approx(1.7467088168).margin(1e-10));
-
-//    setInputs(ml, inputs);
-//    tv = 0.0;
-//    addLoad(ml, tv);
-
-//    norm = ParNormlp(tv, 2.0, MPI_COMM_WORLD);
-//    // std::cout << "norm: " << norm << "\n";
-
-//    REQUIRE(norm == Approx(1.7467088168).margin(1e-10));
-// }
-
 TEST_CASE("MagneticLoad Value Test")
 {
    auto smesh = buildMesh(3, 3);
@@ -300,45 +243,3 @@ mfem::Mesh buildMesh(int nxy, int nz)
    }
    return mesh;
 }
-
-// template <typename xdouble = double>
-// void north_magnetization(const xdouble& remnant_flux,
-//                          const xdouble *x,
-//                          xdouble *M)
-// {
-//    xdouble r[] = {0.0, 0.0, 0.0};
-//    r[0] = x[0];
-//    r[1] = x[1];
-//    xdouble norm_r = sqrt(r[0]*r[0] + r[1]*r[1]);
-//    M[0] = r[0] * remnant_flux / norm_r;
-//    M[1] = r[1] * remnant_flux / norm_r;
-//    M[2] = 0.0;
-// }
-
-// void northMagnetizationSource(const Vector &x,
-//                               Vector &M)
-// {
-//    constexpr auto remnant_flux = 1.0;
-//    north_magnetization(remnant_flux, x.GetData(), M.GetData());
-// }
-
-// template <typename xdouble = double>
-// void south_magnetization(const xdouble& remnant_flux,
-//                          const xdouble *x,
-//                          xdouble *M)
-// {
-//    xdouble r[] = {0.0, 0.0, 0.0};
-//    r[0] = x[0];
-//    r[1] = x[1];
-//    xdouble norm_r = sqrt(r[0]*r[0] + r[1]*r[1]);
-//    M[0] = -r[0] * remnant_flux / norm_r;
-//    M[1] = -r[1] * remnant_flux / norm_r;
-//    M[2] = 0.0;
-// }
-
-// void southMagnetizationSource(const Vector &x,
-//                               Vector &M)
-// {
-//    constexpr auto remnant_flux = 1.0;
-//    south_magnetization(remnant_flux, x.GetData(), M.GetData());
-// }
