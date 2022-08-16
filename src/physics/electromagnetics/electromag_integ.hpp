6a77ee9b1c9d1ef1f322e063006ee4c3bd5c01c7
Tucker Babcock <tbabcock03@gmail.com>
2022-08-16 14:06:31 -0600

    removing ForceIntegrator2 (which was used to test a new implementation but didn't end up working)

--- src/physics/electromagnetics/electromag_integ.hpp
+++ src/physics/electromagnetics/electromag_integ.hpp
@@ -1242,58 +1242,6 @@ private:
 #endif
 };
 
-class ForceIntegrator2 : public mfem::NonlinearFormIntegrator
-{
-public:
-   ForceIntegrator2(StateCoefficient &nu, mfem::GridFunction &v) : nu(nu), v(v)
-   { }
-
-   /// \brief - Compute forces/torques based on the virtual work method
-   /// \param[in] nu - model describing reluctivity
-   /// \param[in] v - the grid function containing virtual displacements for
-   ///                each mesh node
-   /// \param[in] attrs - the regions the force is acting on
-   ForceIntegrator2(StateCoefficient &nu,
-                    mfem::GridFunction &v,
-                    std::unordered_set<int> attrs)
-    : nu(nu), v(v), attrs(std::move(attrs))
-   { }
-
-   /// \brief - Compute element contribution to global force/torque
-   /// \param[in] el - the finite element
-   /// \param[in] trans - defines the reference to physical element mapping
-   /// \param[in] elfun - state vector of the element
-   /// \returns the element contribution to global force/torque
-   double GetElementEnergy(const mfem::FiniteElement &el,
-                           mfem::ElementTransformation &trans,
-                           const mfem::Vector &elfun) override;
-
-   /// \brief - Computes dJdu, for solving for the adjoint
-   /// \param[in] el - the finite element
-   /// \param[in] trans - defines the reference to physical element mapping
-   /// \param[in] elfun - state vector of the element
-   /// \param[out] elfun_bar - \partial J \partial u for this functional
-   void AssembleElementVector(const mfem::FiniteElement &el,
-                              mfem::ElementTransformation &trans,
-                              const mfem::Vector &elfun,
-                              mfem::Vector &elfun_bar) override;
-
-private:
-   /// material dependent model describing reluctivity
-   StateCoefficient &nu;
-   /// grid function containing virtual displacements for each mesh node
-   mfem::GridFunction &v;
-   /// set of attributes the force is acting on
-   std::unordered_set<int> attrs;
-#ifndef MFEM_THREAD_SAFE
-   mfem::DenseMatrix dshape;
-   mfem::DenseMatrix curlshape, curlshape_dFt, curlshape_dFt_bar;
-   mfem::DenseMatrix PointMat_bar;
-   mfem::Array<int> vdofs;
-   mfem::Vector vfun;
-#endif
-};
-
 /// Functional integrator to compute forces/torques based on the virtual work
 /// method
 class ForceIntegrator : public mfem::NonlinearFormIntegrator
